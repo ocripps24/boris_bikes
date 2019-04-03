1f30af796f851ebdf39161a5994048b639479139
Oliver Cripps <ollycripps@gmail.com>
2019-04-03 16:05:16 +0100

    convert first test to one-liner syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'responds to release bike' do
-    expect(subject).to respond_to :release_bike
-  end
+  it { is_expected.to respond_to :release_bike }
 end
+
+# describe DockingStation do
+#   it 'responds to release bike' do
+#     expect(subject).to respond_to :release_bike
+#   end
+# end
