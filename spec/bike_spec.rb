eafa0cd354a504c180a0342e508de64e691c4865
Oliver Cripps <ollycripps@gmail.com>
2019-04-03 16:15:37 +0100

    update working? test to one-liner syntax

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-  it "is bike working" do
-    expect(subject).to respond_to :working?
-  end
+  it { is_expected.to respond_to :working?}
 end
+
+# describe DockingStation do
+#   it "is bike working" do
+#     expect(subject).to respond_to :working?
+#   end
+# end
