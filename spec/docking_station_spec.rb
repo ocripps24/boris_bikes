6fba44a4cab99e58b0db1c7f445133b4e9827fa4
Oliver Cripps <ollycripps@gmail.com>
2019-04-03 17:21:54 +0100

    release_bike now returns a working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
-end
 
-# describe DockingStation do
-#
-# end
+  it { is_expected.to respond_to :release_bike }
+
+  it 'expected to return a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end 
+
+end
 
 # describe DockingStation do
 #   it 'responds to release bike' do
