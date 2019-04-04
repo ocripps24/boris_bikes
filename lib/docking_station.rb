5d661846d19217d9fe875ed9545658c98f8a7268
Oliver Cripps <ollycripps@gmail.com>
2019-04-04 15:39:46 +0100

    step 12,13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,11 +2,15 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
-    return Bike.new
+    fail "Dock is empty" unless @bike
+    @bike
   end
 
   def dock_bike(bike)
+    fail "Dock is full" if @bike 
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,42 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes' do
+      expect { subject.release_bike }.to raise_error("Dock is empty")
+      # it { is_expected.to raise_error("Dock is empty")}
+    end
+  end
+
+  describe '#dock_bike' do
+    it 'raises an error when full' do
+      subject.dock_bike(Bike.new)
+      expect { subject.dock_bike(Bike.new) }.to raise_error("Dock is full")
+    end
+  end
+
   it 'expected to return a working bike' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
+  it 'shows docked bikes' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+  end
 
 end
 
