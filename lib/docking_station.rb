c0c77bf556ef0dd2c382701657b93e9913110d54
Oliver Cripps <ollycripps@gmail.com>
2019-04-05 14:43:44 +0100

    added srp style full method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -14,8 +14,21 @@ class DockingStation
   end
 
   def dock_bike(bike)
-    fail "Dock is full" if @bike_list.count >= 20
+    fail "Dock is full" if full?
     @bike_list << bike
   end
 
+  def full?
+    @bike_list.count >= 20
+  end
+
+
 end
+
+# Define a full? predicate method that uses some of the dock code to return true or false depending on whether the station is full or not
+
+#  Rewrite the guard condition of your dock method to incorporate your new full? method
+
+#  Do the same for release_bike, using an empty? method
+
+#  Use the private keyword to ensure these methods cannot be called from 'outside' instances of the DockingStation class.
